33499d95c06bea843f7af20a7104eed078969c1e
Uros Tement <ut@scantron.si>
2023-07-20 14:35:29 +0200

    Test example

--- example.py
+++ example.py
@@ -1,14 +1,14 @@
 def run():
     a = "Dodam tekst"
-    a = "Dodam tekst1"
-    a = "Dodam tekst2"
-    a = "Dodam tekst3"
+    a = "Dodam tekst1.1"
+    a = "Dodam tekst2.2"
+    a = "Dodam tekst3.3"
     a = "Dodam tekst4"
     a = "Dodam tekst5"
     a = "Dodam tekst6"
     a = "Dodam tekst7"
     a = "Dodam tekst8"
-    a = "Dodam tekst9"
+    a = "Dodam tekst9.1"
 
 
 def main():
