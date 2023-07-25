e47ec5529af0cdacdf0e2440b3fdc69a093b56af
Uroš Tement <utement@gmail.com>
2023-07-25 12:37:26 +0200

    Wip (#1)
    Test coverage check

--- example.py
+++ example.py
@@ -1,19 +1,21 @@
 def run():
     a = "Dodam tekst"
     a = "Dodam tekst1"
+    a = "Dodam tekst1.1"
     a = "Dodam tekst2"
-    a = "Dodam tekst3"
     a = "Dodam tekst4"
     a = "Dodam tekst5"
     a = "Dodam tekst6"
-    a = "Dodam tekst7"
-    a = "Dodam tekst8"
+    a = "Dodam tekst6.1"
+    a = "Dodam tekst6.2"
+    a = "Dodam tekst6.3"
     a = "Dodam tekst9"
+    a = "Dodam tekst10"
+    a = "Dodam tekst11"
+    a = "Dodam tekst12"
 
 
 def main():
+    # Par komentarjev
+    # pa še kakšen
     run()
-
-
-if __name__ == "__main__":
-    main()
